b4f07154f70be03d353d12c468902d63535ecc0b
Scott Tran <scotttran506@yahoo.com>
2021-04-28 12:46:53 -0500

    added consensus and validation

--- blockchain.py
+++ blockchain.py
@@ -1,19 +1,22 @@
 import hashlib
 import json
-from textwrap import ddent
+import requests
 from time import time
 from uuid import uuid4
-from flask import Flask
+from flask import Flask, jsonify, request
+from urllib.parse import urlparse
+
 
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
+        self.nodes = set()
 
         # Create the genesis block
         self.new_block(previous_hash=1, proof=100)
 
-    def new_block(self, previous_hash=None, proof):
+    def new_block(self, proof, previous_hash=None):
         # Create new block and add to chain
         block = {
             'index': len(self.chain) + 1,
@@ -37,18 +40,6 @@ class Blockchain(object):
         })
         return self.last_block['index'] + 1
 
-    @staticmethod
-    def hash(block):
-        # Hash a block
-
-        block_string = json.dumps(block, sort_keys=True).encode()
-        return hashlib.sha256(block_string).hexdigest()
-
-    @property
-    def last_block(self):
-        # Return the last block in the chain
-        return self.chain[-1]
-
     def proof_of_work(self, last_proof):
         """
         Simple Proof of Work Algorithm:
@@ -62,11 +53,79 @@ class Blockchain(object):
 
         return proof
 
+    def register_node(self, address):
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
+
+    def valid_chain(self, chain):
+        last_block = chain[0]
+        current_index = 1
+
+        while current_index < len(chain):
+            block = chain[current_index]
+            print(f'{last_block}')
+            print(f'{block}')
+            print("\n------------\n")
+            # Check that the hash of the block is correct
+            if block['previous_hash'] != self.hash(last_block):
+                return False
+
+            # Check that the Proof of Work is correct
+            if not self.valid_proof(last_block['proof'], block['proof']):
+                return False
+
+            last_block = block
+            current_index += 1
+
+        return True
+
+    def resolve_conflicts(self):
+        # Resolve conflicts by replacing our chain with the largest one in the network
+
+        neighbours = self.nodes
+        new_chain = None
+
+        max_length = len(self.chain)
+
+        # Verify chains from all nodes in our network
+        for node in neighbours:
+            response = requests.get(f'http//{node}/chain')
+
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+
+                # Check if length is longer and chain is valid
+                if length > max_length and self.valid_chain(chain):
+                    max_length = length
+                    new_chain = chain
+
+
+        # Replace chain with longer one
+        if new_chain:
+            self.chain = new_chain
+            return True
+
+        return False
+
+    @property
+    def last_block(self):
+        # Return the last block in the chain
+        return self.chain[-1]
+
+    @staticmethod
+    def hash(block):
+        # Hash a block
+
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
+
     @staticmethod
     def valid_proof(last_proof, proof):
         guess = f'{last_proof}{proof}'.encode()
         guess_hash = hashlib.sha256(guess).hexdigest()
         return guess_hash[:6] == "000000"
+
 
 # Instantiate Node
 app = Flask(__name__)
@@ -75,13 +134,47 @@ node_identifier = str(uuid4()).replace('-', '')
 
 blockchain = Blockchain()
 
+
 @app.route('/mine', methods=['GET'])
 def mine():
-    return "We will mine a new block"
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+
+    blockchain.new_transaction(
+        sender="0",
+        recipient=node_identifier,
+        amount=1,
+    )
+
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.new_block(proof, previous_hash)
+
+    response = {
+        'message': "New Block Forged",
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+    }
+
+    return jsonify(response), 200
+
 
 @app.route('/transactions/new', methods=['POST'])
 def new_transaction():
-    return "We will add a new transaction"
+    values = request.get_json()
+
+    required = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+
+    # Create a new transaction
+    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+
+    response = {'message': f'Transaction will be added to Block {index}'}
+    return jsonify(response), 201
+
 
 @app.route('/chain', methods=['GET'])
 def full_chain():
@@ -90,6 +183,44 @@ def full_chain():
         'length': len(blockchain.chain),
     }
     return jsonify(response), 200
+
+
+@app.route('/nodes/register', methods=['POST'])
+def register_nodes():
+    values = request.get_json()
+
+    nodes = values.get('nodes')
+    if nodes is None:
+        return "Error: Please supply a valid list of nodes", 400
+
+    for node in nodes:
+        blockchain.register_node(node)
+
+    response = {
+        'message': 'New nodes have been added',
+        'total_nodes': list(blockchain.nodes),
+    }
+
+    return jsonify(response), 201
+
+
+@app.route('/nodes/resolve', methods=['GET'])
+def consensus():
+    replaced = blockchain.resolve_conflicts()
+
+    if replaced:
+        response = {
+            'message': 'Our chain was replaced',
+            'new_chain': blockchain.chain
+        }
+    else:
+        response = {
+            'message': 'Our chain is authoritative',
+            'chain': blockchain.chain
+        }
+
+    return jsonify(response), 200
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
